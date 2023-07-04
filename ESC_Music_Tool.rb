7ffa09f1db690be597eb32648068cf782cc078c1
MrD-RC <darren@darrenlines.uk>
2023-07-04 22:46:04 +0100

    Bluejay to BLHeli conversion

--- ESC_Music_Tool.rb
+++ ESC_Music_Tool.rb
@@ -6,12 +6,20 @@ def firstInteger?(x)
     x[/\d+/]
 end
 
+def firstChar?(x)
+    x.index(/[a-z]/)
+end
+
 def titleize?(str)
     str.split(/ |\_/).map(&:capitalize).join(" ")
   end
 
 def findBPM?(blhNoteLength)
     145*blhNoteLength**3/-192+1895*blhNoteLength**2/64-77075*blhNoteLength/192+138125/64
+end
+
+def findGenLength?(bpm)
+    -1777*bpm**3/14000910000+128449*bpm**2/466697000-12806713*bpm/70004550+21242435/466697
 end
 
 def nearest?(a,x)
@@ -30,7 +38,7 @@ outputFirmware = "BLHeli32"
 print "Are you convering from BLHeli32 to BlueJay? (Y/N): "
 toBluejay = if gets.chomp.upcase == 'Y' then true else false end
 
-if toBluejay
+if toBluejay # BLHeli32 to Bluejay
     outputFirmware = "Bluejay"
     octaveCalc = [];
     
@@ -113,9 +121,75 @@ if toBluejay
     notation = notation[0..-2]
 
     notation = "#{tuneName}:o=#{avgOctave},d=#{noteLength},b=#{findBPM?(noteLength)}:#{notation}"
-else
+else # Bluejay to BLHeli32
     puts "Enter the BlueJay Music notation"
     notation = gets.chomp
+
+    durations = {1=>1, 2=>2, 3=>4, 4=>8, 5=>16, 6=>32}
+    defaultOctave = 5
+    defaultDuration = 8
+    bpm = 120
+
+    notationParts = notation.split(':')
+    title = notationParts[0]
+    
+    notationParams = notationParts[1].split(',')
+    notationParams.each do |i|
+        case i[0,2].downcase
+            when "o="
+                defaultOctave = i[2..-1].to_i
+            when "d="
+                defaultDuration = i[2..-1].to_i
+            when "b="
+                bpm = i[2..-1].to_i
+        end
+    end
+
+    durationOffset = 0
+    while defaultDuration > 8
+        durationOffset += 1
+        defaultDuration /= 2
+    end
+
+    if bpm < 250 then
+        interval = 250 - bpm
+        if interval < 250 then interval = 250 end
+        
+        genLength = findGenLength?(250)
+        genInterval = findGenLength?(interval)
+    else
+        genLength = findGenLength?(bpm)
+        genInterval = 0
+    end
+
+    puts "Name: #{title} | Gen. Length #{genLength} | Gen. Interval #{genInterval}"
+
+    notation = ""
+    notes = notationParts[2].split(',')
+    notes.each do |i|
+        fc = firstChar?(i)
+        fi = firstInteger?(i[fc..-1])
+        lastIndex = -1 
+        
+        if i[-1] == '.' then lastIndex = -2 end
+
+        notation += i[fc..lastIndex].upcase
+
+        if fi == nil then notation += defaultOctave end
+        
+        if fc == 0 then 
+            notation += " #{defaultDuration.to_s} "
+        else
+            durKey = durations.key(i[0..fc-1].to_i)
+            durKey -= durationOffset
+            if durKey < 1 then durKey = 1 end
+            notation += " #{durations[durKey]} "
+        end
+
+        if lastIndex == -2 then
+            notation += "P#{defaultDuration} "
+        end
+    end
 end
 
 puts "\nHere are the new notes for #{outputFirmware}"
